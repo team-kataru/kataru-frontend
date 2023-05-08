916ea87b5c1913011dbcf724fba739e376564824
jonlld <87318928+jonlld@users.noreply.github.com>
2023-05-08 22:43:06 +0900

    chore: Test image fetching from Sass file using url

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./sass/main.scss";
 import girl from "./img/girl_reader.png";
+import boy from "./img/boy_reader.png";
 
 function App() {
   return (
-    <div className="container">
+    <div className="header">
       <h1>Kataru!</h1>
-      <img src={girl} style={{ maxWidth: "50%" }} alt="girl reading" />
+      {/* <img src={girl} style={{ maxWidth: "50%" }} alt="girl reading" />
+      <img src={boy} style={{ maxWidth: "50%" }} alt="boy reading" /> */}
     </div>
   );
 }
