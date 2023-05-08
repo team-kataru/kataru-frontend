6816aad91556fce14cb01a5069a0519ce5452c67
jonlld <87318928+jonlld@users.noreply.github.com>
2023-05-08 23:18:42 +0900

    feat: Add initial placeholders for hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./sass/main.scss";
-import girl from "./img/girl_reader.png";
-import boy from "./img/boy_reader.png";
+
+// example image import for TSX
+// import girlReader from "./img/girl_reader.png";
 
 function App() {
   return (
     <div className="header">
-      <h1>Kataru!</h1>
-      {/* <img src={girl} style={{ maxWidth: "50%" }} alt="girl reading" />
-      <img src={boy} style={{ maxWidth: "50%" }} alt="boy reading" /> */}
+      <div className="header__hero"></div>
+      <div className="header__text">
+        <h1>Kataru</h1>
+      </div>
     </div>
   );
 }
