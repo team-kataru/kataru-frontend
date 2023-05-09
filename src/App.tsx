948b49cc32b8ee8cb80853681489be73625f9c83
jonlld <87318928+jonlld@users.noreply.github.com>
2023-05-09 14:18:44 +0900

    feat: Complete initial take of hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import "./sass/main.scss";
 
-// example image import for TSX
+// JSX image import
 // import girlReader from "./img/girl_reader.png";
 
 function App() {
   return (
-    <div className="header">
-      <div className="header__hero"></div>
-      <div className="header__text">
-        <h1>Kataru</h1>
+    <section className="hero">
+      <div className="hero__left">
+        <div className="hero__nav">
+          <a href="#" className="hero__nav--link">
+            Be Inspired
+          </a>
+        </div>
+        <div className="hero__main">
+          <h1 className="hero__heading-primary">
+            <span className="hero__heading-primary--main">Kataru::</span>
+            <span className="hero__heading-primary--sub">
+              Create <span className="u-highlight-2">your</span> masterpiece
+            </span>
+          </h1>
+          <h2 className="hero__heading-secondary">
+            &rarr; One prompt at a time
+          </h2>
+        </div>
+        <div className="hero__detail">
+          <p className="hero__detail--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            blanditiis quasi corporis fugit pariatur alias totam, dolor,
+            possimus nostrum, at facilis? Officia vel voluptas laborum quae
+            rerum tempore quam laboriosam?
+          </p>
+        </div>
       </div>
-    </div>
+      <div className="hero__right"></div>
+    </section>
   );
 }
 
