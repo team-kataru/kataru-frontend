279f7c0078a9dfa0899c8af6c6d069c01d004b14
jonlld <87318928+jonlld@users.noreply.github.com>
2023-05-09 15:44:28 +0900

    feat: Add slideshow to hero with useState and useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import { useState, useEffect } from "react";
 import "./sass/main.scss";
 
-// JSX image import
 import doc from "./img/icons/doc.png";
 
+// Hero images
+import image1 from "./img/hero1.png";
+import image2 from "./img/hero2.png";
+import image3 from "./img/hero3.png";
+import image4 from "./img/hero4.png";
+import image5 from "./img/hero5.png";
+import image6 from "./img/hero6.png";
+import image7 from "./img/hero7.png";
+
 function App() {
+  const [currentImg, setCurrentImg] = useState(0);
+
+  // Array of images to loop
+  const images = [image1, image2, image3, image4, image5, image6, image7];
+
+  // To update current image
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImg((currentImg) => {
+        return currentImg === images.length - 1 ? 0 : currentImg + 1;
+      });
+    }, 5000);
+
+    // clear interval on unmount
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
     <section className="hero">
       <div className="hero__left">
@@ -11,11 +37,7 @@ function App() {
           <a href="#" className="hero__nav--link">
             Sign me up!
           </a>
-          <img
-            src={doc}
-            alt="Image of a colourful pencil"
-            className="hero__nav--img"
-          />
+          <img src={doc} alt="Image of Notebook" className="hero__nav--img" />
         </div>
         <div className="hero__main">
           <h1 className="hero__heading-primary">
@@ -37,7 +59,12 @@ function App() {
           </p>
         </div>
       </div>
-      <div className="hero__right"></div>
+      <div
+        className="hero__right"
+        style={{
+          backgroundImage: `linear-gradient(to right bottom, rgba(131, 56, 236, 0.4), rgba(58, 134, 255, 0.7)), url(${images[currentImg]})`,
+        }}
+      ></div>
     </section>
   );
 }
