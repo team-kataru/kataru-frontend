d5a841c6680768adf0d0a205151a01242b3ef901
jonlld <87318928+jonlld@users.noreply.github.com>
2023-05-11 15:00:48 +0900

    feat: Add useRef to features section and select grid

--- src/components/lp/Features.tsx
+++ src/components/lp/Features.tsx
@@ -1,10 +1,16 @@
+import { useRef, useState } from "react";
+import useObserver from "../../hooks/useObserver";
+
 function Features() {
+  const gridRef = useRef<HTMLDivElement>(null);
+  console.log(gridRef);
+
   return (
     // id for smooth-scrolling link
     <section id="about-scroll" className="features">
       <h2 className="features__title">HOW DOES IT WORK?</h2>
 
-      <div className="features__grid">
+      <div ref={gridRef} className="features__grid">
         <div className="features__grid--item">Row 1 Left</div>
         <div className="features__grid--item">Row 1 Right</div>
         <div className="features__grid--item">Row 2 Left</div>
