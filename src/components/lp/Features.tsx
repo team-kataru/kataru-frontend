52ecb2ac712097178c686899f98962e751cc11de
jonlld <87318928+jonlld@users.noreply.github.com>
2023-05-11 13:10:31 +0900

    feat: Add placeholder grid cards

--- src/components/lp/Features.tsx
+++ src/components/lp/Features.tsx
@@ -1,7 +1,17 @@
 function Features() {
   return (
-    <section id="about-scroll" className="about">
-      <div>HOW DOES IT WORK?</div>
+    // id for smooth-scrolling link
+    <section id="about-scroll" className="features">
+      <h2 className="features__title">HOW DOES IT WORK?</h2>
+
+      <div className="features__grid">
+        <div className="features__grid--item">Row 1 Left</div>
+        <div className="features__grid--item">Row 1 Right</div>
+        <div className="features__grid--item">Row 2 Left</div>
+        <div className="features__grid--item">Row 2 Right</div>
+        <div className="features__grid--item">Row 3 Left</div>
+        <div className="features__grid--item">Row 3 Right</div>
+      </div>
     </section>
   );
 }
