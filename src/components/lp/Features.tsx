81e4725ccc21006bb2a5a2e40cc132593d019fc1
jonlld <87318928+jonlld@users.noreply.github.com>
2023-05-11 16:13:22 +0900

    feat: Test calling useObserver twice with different cells

--- src/components/lp/Features.tsx
+++ src/components/lp/Features.tsx
@@ -2,28 +2,37 @@ import { useRef, useState } from "react";
 import useObserver from "../../hooks/useObserver";
 
 function Features() {
-  const gridRef = useRef<HTMLDivElement>(null);
-  const [visible, setVisible] = useState(false);
+  const leftCellRef = useRef<HTMLDivElement>(null);
+  const rightCellRef = useRef<HTMLDivElement>(null);
+  const [leftVisible, setLeftVisible] = useState(false);
+  const [rightVisible, setRightVisible] = useState(false);
 
-  // Invoke custom hook to mount observer, calling makeVisible when element intersects
-  const makeVisible = () => setVisible(true);
-  useObserver(gridRef, makeVisible, { rootMargin: "-120px 0px" });
+  // Invoke custom hook to mount observers, calling makeVisible when elements intersect
+  const makeLeftVisible = () => setLeftVisible(true);
+  const makeRightVisible = () => setRightVisible(true);
+  const options = { rootMargin: "-120px 0px" };
+  useObserver(leftCellRef, makeLeftVisible, options);
+  useObserver(rightCellRef, makeRightVisible, options);
 
   return (
     // id for smooth-scrolling link
     <section id="about-scroll" className="features">
       <h2 className="features__title">HOW DOES IT WORK?</h2>
-
-      <div
-        ref={gridRef}
-        className={`features__grid ${visible ? "fade-in" : ""}`}
-      >
-        <div className="features__grid--item">Row 1 Left</div>
+      <div className="features__grid">
+        <div
+          ref={leftCellRef}
+          className={`features__grid--item ${leftVisible ? "fade-in" : ""}`}
+        >
+          Row 1 Left
+        </div>
         <div className="features__grid--item">Row 1 Right</div>
         <div className="features__grid--item">Row 2 Left</div>
-        <div className="features__grid--item">Row 2 Right</div>
-        <div className="features__grid--item">Row 3 Left</div>
-        <div className="features__grid--item">Row 3 Right</div>
+        <div
+          ref={rightCellRef}
+          className={`features__grid--item ${rightVisible ? "fade-in" : ""}`}
+        >
+          Row 2 Right
+        </div>
       </div>
     </section>
   );
