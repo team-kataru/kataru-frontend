51ee08315696fb639fb831c9053ce2e283965cf4
jonlld <87318928+jonlld@users.noreply.github.com>
2023-05-16 16:28:20 +0900

    feat: Draft initial layout for feature card

--- src/components/lp/FeatureCard.tsx
+++ src/components/lp/FeatureCard.tsx
@@ -3,20 +3,33 @@ import { useRef, useState } from "react";
 // Using custom hook, but react-intersection-observer library also exists
 import useObserver from "../../hooks/useObserver";
 
-function FeatureCard() {
+type Props = {
+  title: string;
+  img: string;
+  text: string;
+};
+
+function FeatureCard({ title, img, text }: Props) {
   const cardRef = useRef<HTMLDivElement>(null);
   const [isVisible, setIsVisible] = useState(false);
 
   const makeVisible = () => setIsVisible(true);
   useObserver(cardRef, makeVisible, { rootMargin: "-120px 0px" });
 
+  const classes = `card__cell ${isVisible ? "fade-in-on-scroll" : ""}`;
+
   return (
     <div className="card">
-      <div
-        ref={cardRef}
-        className={`card__cell ${isVisible ? "fade-in-on-scroll" : ""}`}
-      >
-        This is a Features Card
+      <div ref={cardRef} className={classes}>
+        <h3 className="card__title">{title}</h3>
+        <div className="card__content">
+          <p>{text}</p>
+          <img
+            className="title__img"
+            src={process.env.PUBLIC_URL + "/img/hero/hero1.png"}
+            alt="Card Image"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/lp/Features.tsx
+++ src/components/lp/Features.tsx
@@ -1,15 +1,44 @@
 import FeatureCard from "./FeatureCard";
 
+// Static content for feature cards
+const FEATURES_DATA = [
+  {
+    title: "This is a test title",
+    img: "/img/hero/",
+    text: "This is some test content. For example, choose your story!",
+  },
+  {
+    title: "This is another test title",
+    img: "/img/hero/",
+    text: "This is some test content. For example, choose your story!",
+  },
+  {
+    title: "This is a third test title",
+    img: "/img/hero/",
+    text: "This is some test content. For example, choose your story!",
+  },
+  {
+    title: "This is a fourth test title",
+    img: "/img/hero/",
+    text: "This is some test content. For example, choose your story!",
+  },
+];
+
 function Features() {
   return (
     // id for scrolling link
     <section id="about-scroll" className="features">
       <h2 className="features__title">HOW DOES IT WORK?</h2>
       <div className="features__grid">
-        <FeatureCard />
-        <FeatureCard />
-        <FeatureCard />
-        <FeatureCard />
+        {FEATURES_DATA.map((feature) => {
+          return (
+            <FeatureCard
+              title={feature.title}
+              img={feature.img}
+              text={feature.text}
+            />
+          );
+        })}
       </div>
     </section>
   );
